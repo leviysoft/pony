ca070b330fdfaebfe203886c226afa684d27d1ad
Daniil Smirnov <danslapman@gmail.com>
2014-07-20 18:32:09 +0400

    Some more controller interfaces for error handling support

--- Pony/ControllerInterfaces/BasicInterfaces.cs
+++ Pony/ControllerInterfaces/BasicInterfaces.cs
@@ -19,4 +19,51 @@ namespace Pony.ControllerInterfaces
     {
         OperationResult<T> Edit(IView<T> view);
     }
+
+    /// <summary>
+    /// Supports handling errors during operations with T
+    /// This method will be hit instead of OnAbort, OnCancel, OnIgnore or OnRetry
+    /// if there if no specific interface that handles such dialog result.
+    /// </summary>
+    /// <typeparam name="T">Model type</typeparam>
+    public interface IHandlesErrors<T> where T : class
+    {
+        OperationResult<T> OnError(IView<T> view);
+    }
+
+    /// <summary>
+    /// Supports handling "Abort" dialog result during operations with T
+    /// </summary>
+    /// <typeparam name="T">Model type</typeparam>
+    public interface IHandlesAbort<T> where T : class
+    {
+        OperationResult<T> OnAbort(IView<T> view);
+    }
+
+    /// <summary>
+    /// Supports handling "Cancel" dialog result during operations with T
+    /// </summary>
+    /// <typeparam name="T">Model type</typeparam>
+    public interface IHandlesCancel<T> where T : class
+    {
+        OperationResult<T> OnCancel(IView<T> view);
+    }
+
+    /// <summary>
+    /// Supports handling "Ignore" dialog result during operations with T
+    /// </summary>
+    /// <typeparam name="T">Model type</typeparam>
+    public interface IHandlesIgnore<T> where T : class
+    {
+        OperationResult<T> OnIgnore(IView<T> view);
+    }
+
+    /// <summary>
+    /// Supports handling "Retry" dialog result during operations with T
+    /// </summary>
+    /// <typeparam name="T">Model type</typeparam>
+    public interface IHandlesRetry<T> where T : class
+    {
+        OperationResult<T> OnRetry(IView<T> view);
+    }
 }
