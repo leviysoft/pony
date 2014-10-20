676eb7268639234085c7cec4083401e394a601cc
Daniil Smirnov <danslapman@gmail.com>
2014-10-20 18:13:51 +0400

    Исправил опечатку

--- Pony.Windows.Forms/Properties/AssemblyInfo.cs
+++ Pony.Windows.Forms/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Pony.Windows.Forms")]
-[assembly: AssemblyDescription("WinForms view frontend form Pony")]
+[assembly: AssemblyDescription("WinForms view frontend for Pony")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("danslapman")]
 [assembly: AssemblyProduct("Pony.Windows.Forms")]
