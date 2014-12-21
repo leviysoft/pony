713354e62c37c2f1f59ab1566e041956fe4b04a3
Daniil Smirnov <danslapman@gmail.com>
2014-12-21 17:18:33 +0300

    NotEmpty for string property validation

---
+++ Pony/Validation/NotEmptyAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Pony.Validation
+{
+    public class NotEmptyAttribute : PropertyValidationAttribute
+    {
+        public NotEmptyAttribute(string errorMessage) : base(errorMessage)
+        {
+        }
+
+        public override Func<object, bool> GetValidator()
+        {
+            return obj => !string.IsNullOrEmpty(obj as string);
+        }
+    }
+}
